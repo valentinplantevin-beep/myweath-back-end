1a4afe6be0f1ee720b1ec8a8e169f8e44def960e
valentinplantevin-beep <valentin.plantevin@gmail.com>
2026-08-29 14:45:33 +0100

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
   if (req.headers['x-api-key'] !== process.env.SYNC_SECRET) {
     return res.status(401).json({ error: 'Non autorisé' });
   }
